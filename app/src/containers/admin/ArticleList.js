08b8b5d61e159ca11b1a013470af43d819dfb9e8
bnewman <Feb252016>
2016-06-08 11:36:54 -0400

    Bootstrap intergration.

--- app/src/containers/admin/ArticleList.js
+++ app/src/containers/admin/ArticleList.js
@@ -39,52 +39,58 @@ export default class ArticleList extends Component {
         {articles, articleTypes, pageList} = articleList.data.data;
 
       return (
-        <div className="main">
+        <div className="container-fluid">
           <PageHeader>Post Administration</PageHeader>
-          <Button><Link to={ADMINPATH + 'article'}>Add Post</Link></Button>
-          <Alert data={article.deleteData} loading={article.deleteing} error={article.deleteError} showAlert={this.state.showAlert} />
-          <div className="table2_wrap">
-            <table className="table2">
-              <tbody>
-              <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Visits</th>
-                <th>Type</th>
-                <th>Tags</th>
-                <th>Comments</th>
-                <th>Created</th>
-                <th>Edited</th>
-                <th>Status</th>
-                <th>Actions</th>
-              </tr>
-              {articles.map((article, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{(pageList.current - 1) * pageList.size + i + 1}</td>
-                    <td>{article.title.slice(0, 25) + (article.title.length > 25 ? '...' : '')}</td>
-                    <td>{article.author}</td>
-                    <td>{article.visits}</td>
-                    <td>{article.type.name}</td>
-                    <td>
-                      {article.tags.map((tag, i) => {
-                        return (i !== 0 ? '、' : '') + tag.name
-                      })}
-                    </td>
-                    <td><Link to={ADMINPATH + 'commentList'} query={{'article.id': article._id}}>{article.commentCount}</Link></td>
-                    <td>{article.createTime.slice(0, 10)}</td>
-                    <td>{article.lastEditTime.slice(0, 10)}</td>
-                    <td>{article.enabled ? 'Enabled' : 'Disabled'}</td>
-                    <td>
-                      <Link to={ADMINPATH + 'article'} query={{id: article._id}}>Edit</Link>&nbsp;&nbsp;
-                      <a href="javascript:void(0)" onClick={this.handleDelete.bind(this, article._id)}>Delete</a>
-                    </td>
-                  </tr>
-                )
-              })}
-              </tbody>
-            </table>
+          <div className="row">
+            <div className="col-md-12">
+              <Button className="btn-primary"><Link to={ADMINPATH + 'article'}>Add Post</Link></Button>
+              <Alert data={article.deleteData} loading={article.deleteing} error={article.deleteError} showAlert={this.state.showAlert} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 m-t-10">
+              <table className="table">
+                <tbody>
+                <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Author</th>
+                  <th>Visits</th>
+                  <th>Type</th>
+                  <th>Tags</th>
+                  <th>Comments</th>
+                  <th>Created</th>
+                  <th>Edited</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+                {articles.map((article, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{(pageList.current - 1) * pageList.size + i + 1}</td>
+                      <td>{article.title.slice(0, 25) + (article.title.length > 25 ? '...' : '')}</td>
+                      <td>{article.author}</td>
+                      <td>{article.visits}</td>
+                      <td>{article.type.name}</td>
+                      <td>
+                        {article.tags.map((tag, i) => {
+                          return (i !== 0 ? '、' : '') + tag.name
+                        })}
+                      </td>
+                      <td><Link to={ADMINPATH + 'commentList'} query={{'article.id': article._id}}>{article.commentCount}</Link></td>
+                      <td>{article.createTime.slice(0, 10)}</td>
+                      <td>{article.lastEditTime.slice(0, 10)}</td>
+                      <td>{article.enabled ? 'Enabled' : 'Disabled'}</td>
+                      <td>
+                        <Link to={ADMINPATH + 'article'} query={{id: article._id}}>Edit</Link>&nbsp;&nbsp;
+                        <a href="javascript:void(0)" onClick={this.handleDelete.bind(this, article._id)}>Delete</a>
+                      </td>
+                    </tr>
+                  )
+                })}
+                </tbody>
+              </table>
+            </div>
           </div>
           <PageList {...pageList} path={ADMINPATH + 'articleList'} />
         </div>
