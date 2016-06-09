02cf265bac8af06668eb27df0ce0fd8459ebe2a1
bnewman <Feb252016>
2016-06-09 14:50:51 -0400

    More bootstrap !

--- app/src/containers/admin/ArticleType.js
+++ app/src/containers/admin/ArticleType.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import connectData from '../../helpers/connectData';
 import Alert from '../../components/Alert';
 import formatForm from '../../utils/formatForm';
@@ -7,6 +8,9 @@ import { editOver } from '../../utils/actionOver';
 import * as detailActions from '../../redux/modules/admin/articleType';
 import State from './State';
 import { pushState } from 'redux-router';
+
+// Bootstrap components
+import { PageHeader, Button, Form, FormGroup, Col, ControlLabel, ButtonToolbar } from 'react-bootstrap';
 
 function fetchData(getState, dispatch, location) {
   return dispatch(detailActions.load({params: {x: 'articleType', id: location.query.id}}));
@@ -25,45 +29,54 @@ export default class ArticleType extends Component {
     showAlert: false
   }
   render() {
-    let
-      detail = this.props.detail;
+    let detail = this.props.detail;
 
     if (detail.data && detail.data.data) {
       let {xData} = detail.data.data;
       return (
-        <div className="main">
-          <table className="table1">
-            <tbody>
-            <tr>
-              <td className="td1">&nbsp;</td>
-              <td><h2>{xData._id ? 'Article Type' : 'Article Type 2'}</h2></td>
-            </tr>
-            <tr>
-              <td className="td1">Name：</td>
-              <td><input type="text" ref="name" className="form-control" defaultValue={xData.name} /></td>
-            </tr>
-            <tr>
-              <td className="td1">Path：</td>
-              <td><input type="text" ref="path" className="form-control" defaultValue={xData.path} /></td>
-            </tr>
-            <tr>
-              <td className="td1">Enabled：</td>
-              <td>
+        <div className="container-fluid">
+          <PageHeader>{xData._id ? 'Edit Type' : 'Create Type'}</PageHeader>
+
+          <Form horizontal>
+            <FormGroup controlId="formHorizontalName">
+              <Col componentClass={ControlLabel} sm={2}>
+                Type
+              </Col>
+              <Col sm={10}>
+                <input type="text" ref="name" className="form-control" placeholder="Type Name" defaultValue={xData.name} />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPath">
+              <Col componentClass={ControlLabel} sm={2}>
+                Path
+              </Col>
+              <Col sm={10}>
+                <input type="text" ref="path" className="form-control" placeholder="Path" defaultValue={xData.path} />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPath">
+              <Col componentClass={ControlLabel} sm={2}>
+                Enable
+              </Col>
+              <Col sm={10}>
                 <select ref="enabled" defaultValue={xData.enabled} className="form-control">
                   <option value={true}>Enabled</option>
                   <option value={false}>Disabled</option>
                 </select>
-              </td>
-            </tr>
-            <tr>
-              <td className="td1">&nbsp;</td>
-              <td>
-                <a href="javascript:void(0)" className="btn" onClick={this.handleSubmit.bind(this, xData._id)}>Save</a>&nbsp;&nbsp;
+              </Col>
+            </FormGroup>
+
+            <Col md={2}></Col>
+            <Col md={10}>
+              <ButtonToolbar>
+                <Button><Link to={ADMINPATH + 'articleTypeList'}>Back</Link></Button>
+                <Button bsStyle="primary" onClick={this.handleSubmit.bind(this, xData._id)}>Submit</Button>
                 <Alert data={detail.editData} loading={detail.editing} error={detail.editError} validateMsg={this.state.validateMsg} showAlert={this.state.showAlert} />
-              </td>
-            </tr>
-            </tbody>
-          </table>
+              </ButtonToolbar>
+            </Col>
+          </Form>
         </div>
       )
     } else {
