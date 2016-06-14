01e684ec9554a972ae391e63db7d17cfe4e46dda
bnewman <Feb252016>
2016-06-14 12:14:50 -0400

    Continues Bootstrap intergration for admin area.

--- app/src/containers/admin/BlogInfo.js
+++ app/src/containers/admin/BlogInfo.js
@@ -9,6 +9,10 @@ import * as blogInfoActions from '../../redux/modules/admin/blogInfo';
 import State from './State';
 import { pushState } from 'redux-router';
 
+// Bootstrap components
+import { PageHeader, Button, Form, FormGroup, Col, ControlLabel, ButtonToolbar } from 'react-bootstrap';
+
+
 function fetchData(getState, dispatch, location) {
   return dispatch(blogInfoActions.load());
 }
@@ -32,42 +36,60 @@ export default class BlogInfo extends Component {
 
     if (loggedin) {
       return (
-        <div className="main">
-          <table className="table1">
-            <tbody>
-            <tr>
-              <td className="td1">&nbsp;</td>
-              <td><h2>Site Info</h2></td>
-            </tr>
-            <tr>
-              <td className="td1">Title：</td>
-              <td><input type="text" ref="title" className="form-control wd3" defaultValue={blogInfo.title}/></td>
-            </tr>
-            <tr>
-              <td className="td1">Keywords：</td>
-              <td><textarea ref="keywords" className="form-control wd6 hg1" defaultValue={blogInfo.keywords}/></td>
-            </tr>
-            <tr>
-              <td className="td1">Description：</td>
-              <td><textarea ref="description" className="form-control wd6 hg1" defaultValue={blogInfo.description}/></td>
-            </tr>
-            <tr>
-              <td className="td1">Copyright：</td>
-              <td><textarea ref="copyright" className="form-control wd6 hg1" defaultValue={blogInfo.copyright}/></td>
-            </tr>
-            <tr>
-              <td className="td1">&nbsp;</td>
-              <td>
-                <a href="javascript:void(0)" className="btn" onClick={this.handleSubmit.bind(this, blogInfo._id)}>Save</a>&nbsp;&nbsp;
+        <div className="container-fluid">
+          <PageHeader>Blog Administration</PageHeader>
+
+          <Form horizontal>
+            <FormGroup controlId="formHorizontalTitle">
+              <Col componentClass={ControlLabel} sm={2}>
+                Title
+              </Col>
+              <Col sm={10}>
+                <input type="text" ref="title" className="form-control" placeholder="Title" defaultValue={blogInfo.title} />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalKeywords">
+              <Col componentClass={ControlLabel} sm={2}>
+                Keywords
+              </Col>
+              <Col sm={10}>
+                <textarea ref="keywords" className="form-control" defaultValue={blogInfo.keywords}/>
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalDescription">
+              <Col componentClass={ControlLabel} sm={2}>
+                Description
+              </Col>
+              <Col sm={10}>
+                <textarea ref="description" className="form-control wd6 hg1" defaultValue={blogInfo.description}/>
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalCopyright">
+              <Col componentClass={ControlLabel} sm={2}>
+                Copyright
+              </Col>
+              <Col sm={10}>
+                <textarea ref="copyright" className="form-control wd6 hg1" defaultValue={blogInfo.copyright}/>
+              </Col>
+            </FormGroup>
+
+            <Col md={2}></Col>
+            <Col md={10}>
+              <ButtonToolbar>
+                <Button><Link to={ADMINPATH + 'articleTypeList'}>Back</Link></Button>
+                <Button bsStyle="primary" onClick={this.handleSubmit.bind(this, blogInfo._id)}>Submit</Button>
                 <Alert data={blogInfoProps.editData} loading={blogInfoProps.editing} error={blogInfoProps.editError} validateMsg={this.state.validateMsg} showAlert={this.state.showAlert}/>
-              </td>
-            </tr>
-            </tbody>
-          </table>
+              </ButtonToolbar>
+            </Col>
+
+          </Form>
         </div>
       )
     } else {
-      return <div className="main"><p>Hello,  <Link to={ADMINPATH + 'login'}>Login</Link></p></div>
+      return <div className="container-fluid"><p>Please  <Link to={ADMINPATH + 'login'}>Login</Link> to continue.</p></div>
     }
   }
   handleSubmit(id) {
